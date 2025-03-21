ec9d67d8b80fcdc3ddf7a2c5b032f19bd4bead92
Parth Kachhela <parthkachhela78@gmail.com>
2025-03-21 09:34:05 +0530

    navbar added now comes to the footer

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../Navbar/Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faUserPlus } from "@fortawesome/free-solid-svg-icons";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
@@ -22,6 +25,12 @@ const Navbar = () => {
         </Link>
         <Link to="/about" className="links">
           About
+        </Link>
+        <Link to="/login" className="links login-button">
+          Login <FontAwesomeIcon icon={faUser} />
+        </Link>
+        <Link to="/signup" className="links signup-button">
+          Signup <FontAwesomeIcon icon={faUserPlus} />
         </Link>
       </div>
     </nav>
